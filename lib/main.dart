820df4d23bfbd07a2ac01a5e2788defed596f4ff
shadyelnady <shadyelnady@gmail.com>
2022-06-07 22:06:20 +0200

    Android IOS Web MacOS initialization

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,48 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+/// Requires that a Firebase local emulator is running locally.
+/// See https://firebase.flutter.dev/docs/auth/start/#optional-prototype-and-test-with-firebase-local-emulator-suite
+bool shouldUseFirebaseEmulator = false;
+
+// Requires that the Firebase Auth emulator is running locally
+// e.g via `melos run firebase:emulator`.
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // We're using the manual installation on non-web platforms since Google sign in plugin doesn't yet support Dart initialization.
+  // See related issue: https://github.com/flutter/flutter/issues/96391
+  if (!kIsWeb) {
+    await Firebase.initializeApp();
+  } else {
+    await Firebase.initializeApp(
+      options: const FirebaseOptions(
+          apiKey: "AIzaSyCKifHKpFBiS6qNu3TVNJDc5lhOQ_7viuU",
+          authDomain: "nady-fire.firebaseapp.com",
+          projectId: "nady-fire",
+          storageBucket: "nady-fire.appspot.com",
+          messagingSenderId: "831203464993",
+          appId: "1:831203464993:web:6cd941901372c10fb11060",
+          measurementId: "G-N9FB554D92"
+          // apiKey: 'AIzaSyAgUhHU8wSJgO5MVNy95tMT07NEjzMOfz0',
+          // appId: '1:448618578101:web:0b650370bb29e29cac3efc',
+          // messagingSenderId: '448618578101',
+          // projectId: 'react-native-firebase-testing',
+          // authDomain: 'react-native-firebase-testing.firebaseapp.com',
+          // databaseURL: 'https://react-native-firebase-testing.firebaseio.com',
+          // storageBucket: 'react-native-firebase-testing.appspot.com',
+          // measurementId: 'G-F79DJ0VFGS',
+          ),
+    );
+  }
+
+  if (shouldUseFirebaseEmulator) {
+    await FirebaseAuth.instance.useAuthEmulator('localhost', 9099);
+  }
+
   runApp(const MyApp());
 }
 
@@ -101,6 +143,16 @@ class _MyHomePageState extends State<MyHomePage> {
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headline4,
+            ),
+            // FireBase
+            StreamBuilder<User?>(
+              stream: FirebaseAuth.instance.authStateChanges(),
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  return const Text("Has Data");
+                }
+                return const Text("No Data");
+              },
             ),
           ],
         ),
